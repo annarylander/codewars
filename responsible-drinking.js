9e681aa6984227dfe786e9b0d48afb46b6266697
Anna Rylander <annarylander@AirsomtlhorAnna.lan>
2022-03-23 17:46:41 +0100

    changed arg  name

--- responsible-drinking.js
+++ responsible-drinking.js
@@ -4,14 +4,15 @@ function hydrate(s) {
   const drinks = s.split(" ").filter(function (item) {
     return !isNaN(item);
   });
-  const parsed = drinks.map((elem) => parseInt(elem, 10));
+  console.log(drinks);
+  const parsed = drinks.map((elem) => parseInt(elem));
 
-  sum = parsed.reduce((pv, cv) => pv + cv, 0);
+  sum = parsed.reduce((accumulated, itemValue) => accumulated + itemValue, 0);
 
   if (sum > 1) {
     return `${sum} glasses of water`;
   } else return `${sum} glass of water`;
 }
 
-console.log(hydrate("1 beer, 2 shots, 3 wine"));
+console.log(hydrate("1 beer, 2 shots, 3 wine, 20 irish"));
 //Output: 6 glasses of water
